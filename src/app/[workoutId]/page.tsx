d817ddbbd2b0c94d411bd1ae8b174f4da3e8ab87
eduzrdo <eduardoliveira.dev@gmail.com>
2025-05-12 11:33:41 -0300

    ✨ feature: access workout in the route (e.g. /1)

--- src/app/[workoutId]/page.tsx
+++ src/app/[workoutId]/page.tsx
@@ -1,14 +1,21 @@
-import { ExerciseCard, Header } from "@/src/components";
+"use client";
 
+import { useParams } from "next/navigation";
+
+import { ExerciseCard, Header } from "@/src/components";
 import { workoutPlan } from "@/src/data/workoutPlan";
+import router from "next/router";
 
 export default function Home() {
+  const router = useParams<{ workoutId: string }>();
+  const { workoutId } = router;
+
   return (
     <div className="flex flex-col h-[100dvh] md:max-w-96 mx-auto">
       <Header title="(Push) Dorsal e Bíceps" />
 
       <div className="flex flex-col p-5 gap-5">
-        {workoutPlan[1].exercises.map((exercise) => (
+        {workoutPlan[Number(workoutId)].exercises.map((exercise) => (
           <ExerciseCard key={exercise.id} {...exercise} />
         ))}
       </div>
