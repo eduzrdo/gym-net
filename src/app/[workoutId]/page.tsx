8ecf51000c04a1d26839615ab6b0b56ff3ca1291
eduzrdo <eduardoliveira.dev@gmail.com>
2025-05-21 15:12:20 -0300

    ✨ feature: select workout

--- src/app/[workoutId]/page.tsx
+++ src/app/[workoutId]/page.tsx
@@ -1,23 +1,64 @@
 "use client";
 
+import { useState } from "react";
 import { useParams } from "next/navigation";
+import { IconBarbell, IconX } from "@tabler/icons-react";
+import Link from "next/link";
+import colors from "tailwindcss/colors";
 
 import { ExerciseCard, Header } from "@/src/components";
 import { workoutPlan } from "@/src/data/workoutPlan";
 
 export default function Home() {
+  const [showWorkoutList, setshowWorkoutList] = useState(false);
+
   const router = useParams<{ workoutId: string }>();
   const { workoutId } = router;
 
-  return (
-    <div className="flex flex-col h-[100dvh] md:max-w-96 mx-auto">
-      <Header title="(Push) Dorsal e Bíceps" />
+  const handleShowWorkoutList = () => {
+    setshowWorkoutList(true);
+  };
 
-      <div className="flex flex-col p-5 gap-5">
+  const handleHideWorkoutList = () => {
+    setshowWorkoutList(false);
+  };
+
+  return (
+    <div className="relative flex flex-col h-[100dvh] md:max-w-96 mx-auto">
+      <Header
+        title={workoutPlan[Number(workoutId)].title}
+        onClickHeaderIcon={handleShowWorkoutList}
+      />
+
+      <div className="flex flex-1 flex-col p-5 gap-5 overflow-scroll">
         {workoutPlan[Number(workoutId)].exercises.map((exercise) => (
           <ExerciseCard key={exercise.id} {...exercise} />
         ))}
       </div>
+
+      {showWorkoutList && (
+        <div className="absolute top-0 left-0 w-full h-full bg-black/50 flex flex-col justify-end gap-5">
+          <div className="bg-white rounded-tl-xl rounded-tr-xl px-5 py-2">
+            <ul className="flex flex-col">
+              <li className="flex justify-between items-center gap-3 py-2">
+                <p>Treinos</p>
+                <button onClick={handleHideWorkoutList}>
+                  <IconX color={colors.zinc[500]} size={24} />
+                </button>
+              </li>
+              {workoutPlan.map((workoutPlanItem, index) => (
+                <li
+                  key={workoutPlanItem.title}
+                  className="flex items-center gap-3 py-2"
+                >
+                  <IconBarbell color={colors.zinc[800]} size={24} />
+                  <Link href={`/${index}`}>{workoutPlanItem.title}</Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,24 @@
+import { IconListLetters } from "@tabler/icons-react";
+import colors from "tailwindcss/colors";
+
 type HeaderProps = {
   title: string;
+  onClickHeaderIcon?: () => void;
 };
 
-export function Header({ title }: HeaderProps) {
+export function Header({ title, onClickHeaderIcon }: HeaderProps) {
   return (
-    <div className="p-5 pt-10 bg-green-600 rounded-br-xl rounded-bl-xl sticky top-0">
-      <p className="text-white text-xl font-bold">{title}</p>
+    <div className="flex items-center p-5 pt-10 bg-green-600 rounded-br-xl rounded-bl-xl gap-4">
+      <p className="flex-1 text-white text-xl font-bold overflow-ellipsis truncate">
+        {title}
+      </p>
+
+      <button
+        onClick={onClickHeaderIcon}
+        className="bg-white w-8 h-8 flex justify-center items-center rounded-full active:bg-zinc-100"
+      >
+        <IconListLetters color={colors.green[600]} size={24} />
+      </button>
     </div>
   );
 }
