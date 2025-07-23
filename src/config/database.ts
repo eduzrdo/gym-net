a820101136178e80b8926c51c7cd9404a008c66b
eduzrdo <eduardoliveira.dev@gmail.com>
2025-07-23 15:03:54 -0300

    firestore config

---
+++ src/config/database.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA1mY3Uad41senYZhjwf6xJzOBb7kCpIyY",
+  authDomain: "gymnet-e32e5.firebaseapp.com",
+  projectId: "gymnet-e32e5",
+  storageBucket: "gymnet-e32e5.firebasestorage.app",
+  messagingSenderId: "1035424746582",
+  appId: "1:1035424746582:web:f6e3b90255e028a431fff7",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
